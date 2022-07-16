8cdfc8ec6a8bb3e50614cc8474e4c72383d99b70
piciakk <marci@dbx.hu>
2022-07-16 11:40:40 +0200

    add description, add url, new version

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject app "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject app "0.2.0-SNAPSHOT"
+  :description "egy horoszkópos játék, ami később az astronet.hu-s szövegek igazságtartalmát is fel tudja majd mérni"
+  :url "https://github.com/PiciAkk/horoszkop-kiserlet"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
